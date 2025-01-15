0fc19e7002c9e08c32aaa36cd21e8497ab16ce2a
furaizi <furachannel16@gmail.com>
2025-01-15 22:05:01 +0200

    Add tests for setContent method of TextProcessor.java

--- src/test/java/org/example/model/TextProcessorTest.java
+++ src/test/java/org/example/model/TextProcessorTest.java
@@ -1,6 +1,38 @@
 package org.example.model;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class TextProcessorTest {
 
+    @Test
+    public void testSetRealContent() {
+        var expected = "Hello world!";
+        var model = new TextProcessor();
+        model.setContent(expected);
 
+        var actual = model.getContent();
+        assertNotNull(actual, "Set content should not be null");
+        assertEquals(expected, actual, "Set content should be equal to the origin");
+    }
+
+    @Test
+    public void testSetNullContent() {
+        var model = new TextProcessor();
+        assertThrows(NullPointerException.class, () -> model.setContent(null), "setContent method should throw NullPointerException when null is supplied");
+    }
+
+    @Test
+    public void testForceSetContent() {
+        var oldText = "old text";
+        var newText = "new text";
+        var model = new TextProcessor();
+        model.setContent(oldText);
+        model.setContent(newText);
+
+        var actual = model.getContent();
+        assertNotNull(actual);
+        assertEquals(newText, actual, "setContent method should set new content when supplied, not add");
+    }
 }
